7a8ac2673f71808ffa6b396080ebacf056f9347e
JonasKempeneers <jonaskempeneers193@gmail.com>
2018-04-02 17:30:38 +0200

    Added methods to get data

--- src/test/java/com/pxl/student/BudgetPlannerTest.java
+++ src/test/java/com/pxl/student/BudgetPlannerTest.java
@@ -3,6 +3,9 @@ package com.pxl.student;
 import Utility.BudgetPlannerDB;
 import com.pxl.student.bean.Account;
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -11,21 +14,50 @@ import javax.persistence.Persistence;
 
 public class BudgetPlannerTest extends TestCase {
 
-    public void testAddAccountToDatabase(){
-        String number = BudgetPlannerDB.generateRandomNumber();
-        String IBAN = BudgetPlannerDB.generateIBAN();
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("BudgetPlanner");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-        Account account = new Account(number, IBAN, "Joske");
-        em.persist(account);
-        tx.commit();
-        em.close();
-        emf.close();
+    private EntityManagerFactory _emf;
+    private EntityManager _em;
+    private EntityTransaction _tx;
+    private String _number = BudgetPlannerDB.generateRandomNumber();
+    private String _IBAN = BudgetPlannerDB.generateIBAN();
+    private String _name = "Random Test Accout";
+    Account actualAccount = createAccount();
 
-        Account expectedAccount = account;
-        Account actualAccount;
+    private Account createAccount(){
+        Account account = new Account(_number, _IBAN, _name);
+        return account;
+    }
+
+    @Before
+    public void SetUp(){
+        _emf = Persistence.createEntityManagerFactory("BudgetPlanner");
+        _em = _emf.createEntityManager();
+    }
+
+    @After
+    public void After(){
+        _em.close();
+        _emf.close();
+    }
+
+    @Test
+    public void testAddAccountToDatabase(){
+        _tx = _em.getTransaction();
+        _tx.begin();
+        _em.persist(actualAccount);
+        _tx.commit();
+
+
+    }
+
+    @Test
+    public void testGetAccountFromDatabase(){
+
+        _tx = _em.getTransaction();
+        _tx.begin();
+        Account expectedAccount = actualAccount;
+        actualAccount = _em.find(Account.class, 1L);
+
+        assertEquals(expectedAccount, actualAccount);
     }
 
 }
